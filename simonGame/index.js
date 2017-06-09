e187f874de0b372ac4ba15558bef80c74192d3a8
KristjanTs <kristjantsimmer@gmail.com>
2017-06-09 22:42:25 +0300

    Update index.js

--- simonGame/index.js
+++ simonGame/index.js
@@ -1,48 +1,52 @@
 $(document).ready(function(){
   var numOrder = [];
   var counter= 0;
-  for(var i=0; i<20; i++){
-    numOrder.push(Math.floor(Math.random()*4)+1);
-  }
-  $("#first").click(function(){
-    console.log(1);
-    if(numOrder[counter]!==1){
-      $("#text").html("Nope");
-    }
-    else {
-      counter +=1;
-    }
-  });
   
-  $("#second").click(function(){
-    console.log(2);
-    if(numOrder[counter]!==2){
-      $("#text").html("Nope");
+  $("#start").click(function(){
+      $("#start").hide();
+      for(var i=0; i<20; i++){
+      numOrder.push(Math.floor(Math.random()*4)+1);
     }
-    else {
-      counter +=1;
-    }
+
+    $("#first").click(function(){
+      if(numOrder[counter]!==1){
+        $("#text").html("Nope");
+      }
+      else {
+        counter +=1;
+      }
+    });
+
+    $("#second").click(function(){
+      console.log(2);
+      if(numOrder[counter]!==2){
+        $("#text").html("Nope");
+      }
+      else {
+        counter +=1;
+      }
+    });
+
+    $("#third").click(function(){
+      console.log(3);
+      if(numOrder[counter]!==3){
+        $("#text").html("Nope");
+      }
+      else {
+        counter +=1;
+      }
+    });
+
+    $("#fourth").click(function(){
+      console.log(4);
+      if(numOrder[counter]!==4){
+        $("#text").html("Nope");
+      }
+      else {
+        counter +=1;
+      }
+    });
+    console.log(numOrder);
   });
-  
-  $("#third").click(function(){
-    console.log(3);
-    if(numOrder[counter]!==3){
-      $("#text").html("Nope");
-    }
-    else {
-      counter +=1;
-    }
-  });
-  
-  $("#fourth").click(function(){
-    console.log(4);
-    if(numOrder[counter]!==4){
-      $("#text").html("Nope");
-    }
-    else {
-      counter +=1;
-    }
-  });
-  console.log(numOrder);
   
 });
