0478f0cbc10106f67739414820bd89b1bf211e27
KristjanTs <kristjantsimmer@gmail.com>
2017-07-06 23:33:38 +0300

    Update server.js
    Added $post

--- MEAN_App/server.js
+++ MEAN_App/server.js
@@ -1,9 +1,11 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var mongojs = require("mongojs");
-var db = mongojs("contactList", ["contactList"]);
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get("/contactList", function(req, res){
   console.log("GET request success");
@@ -11,6 +13,12 @@ app.get("/contactList", function(req, res){
   db.contactList.find(function(err, docs){
     console.log(docs);
     res.json(docs);
+  });
+});
+
+app.post('/contactList', function (req, res){
+  db.contactList.insert(req.body, function (err, doc){
+    res.json(doc);
   });
 });
 
