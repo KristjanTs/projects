cf8b0045095ace6844af873522385d9328dae353
KristjanTs <kristjantsimmer@gmail.com>
2017-07-06 23:34:45 +0300

    Update controller.js

--- MEAN_App/Public/controllers/controller.js
+++ MEAN_App/Public/controllers/controller.js
@@ -2,14 +2,31 @@ var myApp = angular.module("myApp",[]);
 
 myApp.controller("AppCtrl",["$scope","$http", function($scope, $http){
   console.log("test controller");
-  $http.get("/contactList").then(successCallback, failCallback);
 
-  function successCallback(res){
-    console.log("Data received");
-    $scope.contactList=res.data;
+  var refresh = function(){
+    $http.get("/contactList").then(successCallback, failCallback);
+
+    function successCallback(res){
+      console.log("Data received");
+      $scope.contactList=res.data;
+      $scope.contact={};
+    };
+
+    function failCallback(err){
+      console.log(err.message);
+    };
+  };
+
+  refresh();
+
+  $scope.addContact = function(){
+  	$http.post("/contactList", $scope.contact).then(function (success){
+
+    },function (error){
+
+	  });
+    refresh();
   }
 
-  function failCallback(err){
-    console.log(err.message);
-  }
+
 }]);
