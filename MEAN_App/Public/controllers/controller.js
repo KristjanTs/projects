57527d0843808a7916234d243d8d31dcc58a47a3
KristjanTs <kristjantsimmer@gmail.com>
2017-07-08 22:43:56 +0300

    Update controller.js

--- MEAN_App/Public/controllers/controller.js
+++ MEAN_App/Public/controllers/controller.js
@@ -34,4 +34,21 @@ myApp.controller("AppCtrl",["$scope","$http", function($scope, $http){
     refresh();
   };
 
+  $scope.edit = function(id){
+    console.log(id);
+    $http.get("/contactList/"+id).then(function(success){
+      $scope.contact = success.data;
+      console.log("yea bae");
+    });
+  }
+
+  $scope.update = function(){
+  	console.log($scope.contact._id);
+    $http.put('/contactlist/'+ $scope.contact._id, $scope.contact).then(function (success){
+      console.log(success.data);
+      refresh();
+      },function (error){
+    });
+  }
+
 }]);
