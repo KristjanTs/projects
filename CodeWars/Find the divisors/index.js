d9ef0848b07ea57786aadeffbb153eae07b687a6
KristjanTs <kristjantsimmer@gmail.com>
2017-07-19 09:38:56 +0300

    Update index.js

--- CodeWars/Find the divisors/index.js
+++ CodeWars/Find the divisors/index.js
@@ -1,14 +1,11 @@
 function divisors(integer) {
-  var count=[];
-  for(var i=2; i<integer; i++){
-    if(integer%i==0){
-      count.push(i);
+  var divs = [];
+  
+  for(var i = 2; i < integer; i++) {
+    if(integer % i === 0) {
+      divs.push(i);
     }
   }
-  if(count.length==0){
-    return integer+" is prime";
-  }
-  else{
-    return count;
-  }
+  
+  return divs.length ? divs : integer + ' is prime';
 };
